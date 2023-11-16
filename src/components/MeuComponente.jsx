f84721bf26dd0442a86d2f624d753e1bfcf6439c
ducampos1 <ducampos1@gmail.com>
2023-11-15 21:40:21 -0300

    primeiro commit

---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,6 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facilis amet nisi natus nobis nemo. Quisquam repellendus architecto iste culpa voluptatum vero deleniti laudantium aut. Nostrum numquam debitis consequuntur! Tempore!</p>
+    )
+  }
+
